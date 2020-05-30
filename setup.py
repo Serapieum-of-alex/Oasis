6aef2f9a0aa535ddf4ee9cbf7bd51fe67ad88007
Mostafa Farrag <eng_mostafa_ahmed23@yahoo.com>
2020-05-30 02:58:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup , find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='Oasis',
-	packages=['Oasis'],
     version='0.1.0',
-	description='Optimization Algorithm',
+    description='Optimization Algorithm',
 	author='Mostafa Farrag',
 	author_email='moah.farag@gmail.come',
-	url='https://github.com/MAfarrag/HAPI',
-    keywords=['Hydrology', 'Distributed hydrological model'],	
+	url='https://github.com/MAfarrag/Oasis',
+    keywords=['Optimization', 'Harmony Search'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 	License="MIT" ,
     zip_safe=False, 
+    packages=find_packages(),
     classifiers=[	
     'Development Status :: 4 - Beta',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Natural Language :: English',
-    'Programming Language :: Python 3.7',
-	'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python',
     'Topic :: Software Development',
     ]
 	 )
