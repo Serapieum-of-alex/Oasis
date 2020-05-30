328a634a06400456d4276614ea3c46770605c747
Mostafa Farrag <eng_mostafa_ahmed23@yahoo.com>
2020-05-30 04:44:50 +0200

    Update setup.py
    change name for pip unique name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='Oasis',
+    name='Oasis-Optimization',
     version='0.1.0',
     description='Optimization Algorithm',
 	author='Mostafa Farrag',
